2e7a1b1b6eb2d0e56ea6e43418bc4a74e202a8f9
jeongmin <jeongmin0046@gmail.com>
2024-11-18 11:39:48 +0900

    mod: 다음 페이지 오류 수정

--- diary/src/pages/Home.jsx
+++ diary/src/pages/Home.jsx
@@ -28,11 +28,11 @@ const Home = () => {
   const headerTitle = `${pivotDate.getFullYear()}년 ${pivotDate.getMonth() +1}월`;
 
   const onIncreaseMonth = () => {
-    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
   }
 
   const onDecreaseMonth = () => {
-    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
   }
 
   return(
